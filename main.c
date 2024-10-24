e24c43aef4b10a31c250f2f588411314c0dfdb48
yanislammari <yanis.lammari937@gmail.com>
2024-10-24 16:28:50 +0200

    init game window

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
-#include "stdio.h"
+#include "src/includes/game.h"
 
 int main(int argc, char* argv[]){
+    run_game();
     return 0;
 }
---
+++ src/game.c
@@ -0,0 +1,27 @@
+#include <SDL.h>
+#include "includes/screen.h"
+#include "includes/game.h"
+
+void run_game() {
+    SDL_Window* window = SDL_CreateWindow("PoCemon", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN);
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    int quit = 0;
+    SDL_Event e;
+
+    while (!quit) {
+        while(SDL_PollEvent(&e) != 0) {
+            if(e.type == SDL_QUIT) {
+                quit = 1;
+            }
+            else if(e.type == SDL_KEYDOWN) {
+                if(e.key.keysym.sym == SDLK_f) {
+                    pleinEcranWindow(window);
+                }
+            }
+        }
+    }
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+}
