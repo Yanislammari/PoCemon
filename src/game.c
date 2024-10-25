ac793c9199c22c48373da6ca480b7d0e147b04d8
yanislammari <yanis.lammari937@gmail.com>
2024-10-25 09:54:29 +0200

    refactor

--- src/game.c
+++ src/game.c
@@ -9,7 +9,7 @@ void run_game() {
     int quit = 0;
     SDL_Event e;
 
-    while (!quit) {
+    while(!quit) {
         while(SDL_PollEvent(&e) != 0) {
             if(e.type == SDL_QUIT) {
                 quit = 1;
