f4f6e75c17322901d7a7554b4a25280806041813
yanislammari <yanis.lammari937@gmail.com>
2024-10-26 14:41:42 +0200

    refactor

--- src/text.c
+++ src/text.c
@@ -1,7 +1,7 @@
 #include <SDL_ttf.h>
 #include "includes/text.h"
 
-void render_text(SDL_Renderer* renderer, TTF_Font* font, char* text, SDL_Color color, int x, int y){
+void render_text(SDL_Renderer* renderer, TTF_Font* font, char* text, SDL_Color color, int x, int y) {
     SDL_Surface* surface = TTF_RenderText_Blended(font, text, color);
     SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
     SDL_Rect dest_rect = {x, y, surface->w, surface->h};
